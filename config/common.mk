LeafOS-Project/android_vendor_leaf

leaf: adblock: Include some more lists
Change-Id: Iaec3be3a9f6d995730d38ff5154978d0bb8a684b

config/common.mk
@@ -22,7 +22,7 @@ PRODUCT_PACKAGES += \
     hosts.real
 
 PRODUCT_COPY_FILES += \
-    vendor/leaf/etc/init/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
+    vendor/leaf/etc/adblock/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
 
 # Apps
 PRODUCT_PACKAGES += \

droid-legacy/android_vendor_lineage

config/common.mk
@@ -41,7 +41,7 @@ PRODUCT_PACKAGES += \
     hosts.real
 
 PRODUCT_COPY_FILES += \
-    vendor/lineage/etc/init/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
+    vendor/lineage/etc/adblock/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
 
 # Backup Tool
 PRODUCT_COPY_FILES += \