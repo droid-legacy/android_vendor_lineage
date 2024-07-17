LeafOS-Project/android_vendor_leaf

leaf: Initial adblock setup
Change-Id: I9574f5f56ba58ba604c9da84e56080a6e528832b

config/common.mk
@@ -15,6 +15,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 
+# Adblock
+PRODUCT_COPY_FILES += \
+    vendor/leaf/etc/hosts.adblock:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/hosts.adblock \
+    vendor/leaf/etc/init/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
+
 # Apps
 PRODUCT_PACKAGES += \
     Etar \

droid-legacy/android_vendor_lineage

Signed-off-by: SirRGB <sirrgb@proton.me>

config/common.mk
@@ -34,6 +34,13 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.adb.secure=1
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += persist.sys.strictmode.disable=true
 endif
 
+# Adblock
+PRODUCT_PACKAGES += \
+    hosts.adblock
+
+PRODUCT_COPY_FILES += \
+    vendor/lineage/etc/init/init.adblock.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.adblock.rc
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \